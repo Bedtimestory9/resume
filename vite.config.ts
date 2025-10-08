7bec4d67ab54c0148a2172869e26d3cc752af00c
Lawrence Su <lawrencefandango@gmail.com>
2025-10-08 13:10:25 +0800

    awdwd

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/resume/",
+  // base: "/resume/",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean,
   ),
